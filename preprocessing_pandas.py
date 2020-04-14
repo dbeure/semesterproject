413925222fbb4567595de8ca4b4f789f9915963a
David Bielik <me@dvdblk.com>
2020-04-15 00:19:37 +0200

    Fix preprocessing one row missing token value

--- preprocessing_pandas.py
+++ preprocessing_pandas.py
@@ -14,9 +14,11 @@ if __name__ == "__main__":
     corpus_df = pd.read_csv(
         data_file,
         sep='\t',
-        skip_blank_lines=True,
-        comment="#",
-        quoting=3
+        keep_default_na=False,      # treats 'null' as a string rather than a number while parsing
+        skip_blank_lines=True,   
+        comment="#",                # ignores lines starting with '#'
+        quoting=3                   # handles quotes as tokens correctly
     )
 
     print(corpus_df.info())
+
